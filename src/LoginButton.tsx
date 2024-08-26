85599fdb42c0ea483ccac8f289c15a635564262d
Aron Schüler <aron.schueler@ambient.digital>
2024-08-26 07:37:16 +0200

    fix: correct text of login/logout buttons

--- src/LoginButton.tsx
+++ src/LoginButton.tsx
@@ -2,19 +2,14 @@ import { useAuth } from "./useAuth";
 
 export const LoginButton = () => {
   const { login, characterName } = useAuth();
+  if (characterName) return null;
 
   return (
-    <div>
-      {!characterName ? (
-        <button onClick={login} style={{
-          padding: "0", border: "none"
-        }}>
-          <img src="/eve-sso-login-black-small.png" alt="Login with EVE Online" />
-          <span style={{ display: "none" }}> Login with EVE Online</span>
-        </button>
-      ) : (
-        <p>Welcome, {characterName}</p>
-      )}
-    </div>
+    <button onClick={login} style={{
+      padding: "0", border: "none"
+    }}>
+      <img src="/eve-sso-login-black-small.png" alt="Login with EVE Online" />
+      <span style={{ display: "none" }}> Login with EVE Online</span>
+    </button>
   );
 };
--- src/LogoutButton.tsx
+++ src/LogoutButton.tsx
@@ -2,15 +2,12 @@ import { useAuth } from "./useAuth";
 
 export const LogoutButton = () => {
   const { logout, characterName } = useAuth();
+  if (!characterName) return null;
 
   return (
-    <div>
-      {characterName && (
-        <>
-          <p>Logged in as {characterName}</p>
-          <button onClick={logout}>Logout</button>
-        </>
-      )}
-    </div>
+    <div style={{ display: "flex", flexDirection: "row", gap: "2rem" }}>
+      <p>Logged in as {characterName}</p>
+      <button onClick={logout}>Logout</button>
+    </div >
   );
 };
